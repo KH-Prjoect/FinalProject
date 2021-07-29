a954f29b0496ed124e04cba71b70cd2227f9c534
hdg1002 <82365459+hdg1002@users.noreply.github.com>
2021-07-29 20:36:14 +0900

    Add files via upload

--- SpringProjectBnpp/src/main/java/com/kh/bnpp/controller/MypageController.java
+++ SpringProjectBnpp/src/main/java/com/kh/bnpp/controller/MypageController.java
@@ -1,3 +1,4 @@
+
 package com.kh.bnpp.controller;
 
 import java.io.UnsupportedEncodingException;
@@ -10,7 +11,8 @@ import javax.servlet.http.HttpServletResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.lang.Nullable;
+import org.springframework.beans.factory.annotation.Value;
+
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.stereotype.Repository;
@@ -19,15 +21,20 @@ import org.springframework.web.bind.annotation.RequestBody;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.kh.bnpp.model.biz.BillBiz;
+import com.kh.bnpp.model.biz.ClassBiz;
+import com.kh.bnpp.model.biz.FileUploadBiz;
 import com.kh.bnpp.model.biz.FoodBiz;
-import com.kh.bnpp.model.biz.LectureBiz;
 import com.kh.bnpp.model.biz.MemberBiz;
 import com.kh.bnpp.model.biz.PayBiz;
+import com.kh.bnpp.model.dto.BillDto;
+import com.kh.bnpp.model.dto.ClassDetailDto;
+import com.kh.bnpp.model.dto.ClassDto;
 import com.kh.bnpp.model.dto.FoodDto;
 import com.kh.bnpp.model.dto.FoodListDto;
-import com.kh.bnpp.model.dto.LectureDto;
 import com.kh.bnpp.model.dto.MemberDto;
 import com.kh.bnpp.model.dto.PayDto;
 import com.kh.bnpp.sms.SMS;
@@ -44,15 +51,31 @@ public class MypageController {
 	private PayBiz p_biz;
 	
 	@Autowired
-	private LectureBiz l_biz;
+	private ClassBiz c_biz;
 	
 	@Autowired
 	private FoodBiz f_biz;
 	
 	@Autowired
-	private BCryptPasswordEncoder passwordEncoder;
+	private BillBiz b_biz;
 	
-	@Nullable
+	@Autowired
+	private BCryptPasswordEncoder passwordEncoder;
+
+	@Value("${imgfile.Uploadpath}")
+	private String imgUploadPath;	//이거는 로컬에 저장되는거 C:\\Workspaces\\Workspace_final\\SpringProjectBnpp\\src\\main\\webapp\\resources\\img\\
+	
+	@Autowired
+	private FileUploadBiz file_biz;
+
+	@RequestMapping("/receiptupload.do")
+	public String receiptupload(Model model, String member_id) {
+		List<BillDto> b_list = b_biz.selectList(member_id);
+		model.addAttribute("member_id", member_id);
+		model.addAttribute("b_list", b_list);
+		return "receiptupload";
+	}
+	
 	@RequestMapping("/mypage.do")
 	public String mypage(String member_id) {
 		
@@ -67,8 +90,7 @@ public class MypageController {
 					try {
 						if (!SMS.compareDate(f_life).equals("0")) {
 							alarm_dto = m_biz.selectOne(f_dto.getMember_id());
-							// phone = alarm_dto.getMember_phone().replace("-", "");
-							phone = "01064244977";
+							phone = alarm_dto.getMember_phone().replace("-", "");
 							content = "내 냉장고 안의 " + f_dto.getFood_name() + "의 " + SMS.compareDate(f_life);
 							try {
 								SMS.sendSMS(phone, content);
@@ -101,14 +123,14 @@ public class MypageController {
 	public String mypage_student(Model model, String member_id) {
 		MemberDto m_dto = m_biz.selectOne(member_id);
 		List<PayDto> p_list = p_biz.selectMyList(member_id);
-		List<LectureDto> l_list = new ArrayList<LectureDto>();
-		LectureDto l_dto = null;
+		List<ClassDto> c_list = new ArrayList<ClassDto>();
+		ClassDto c_dto = null;
 		for (PayDto p_dto : p_list) {
-			l_dto = l_biz.selectOne(p_dto.getLecture_num());
-			l_list.add(l_dto);
+			c_dto = c_biz.selectOne(p_dto.getClass_num());
+			c_list.add(c_dto);
 		}
 		List<FoodDto> f_list = f_biz.selectMyList(member_id);
-		model.addAttribute("l_list", l_list);
+		model.addAttribute("c_list", c_list);
 		model.addAttribute("f_list", f_list);
 		model.addAttribute("m_dto", m_dto);
 		return "mypage_student";
@@ -117,9 +139,9 @@ public class MypageController {
 	@RequestMapping("/mypage_teacher.do")
 	public String mypage_teacher(Model model, String member_id) {
 		MemberDto m_dto = m_biz.selectOne(member_id);
-		List<LectureDto> l_list = l_biz.selectMyList(member_id);
+		List<ClassDetailDto> c_list = c_biz.selectList(member_id);
 		List<FoodDto> f_list = f_biz.selectMyList(member_id);
-		model.addAttribute("l_list", l_list);
+		model.addAttribute("c_list", c_list);
 		model.addAttribute("f_list", f_list);
 		model.addAttribute("m_dto", m_dto);
 		return "mypage_teacher";
@@ -187,5 +209,6 @@ public class MypageController {
 	}
 	
 }
+
 
 
