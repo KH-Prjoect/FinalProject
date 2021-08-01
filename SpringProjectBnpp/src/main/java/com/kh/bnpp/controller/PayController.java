3a20c38a5ab6f2a0ff47020e6a3fa56590067387
hdg1002 <82365459+hdg1002@users.noreply.github.com>
2021-08-01 18:39:48 +0900

    Add files via upload

--- SpringProjectBnpp/src/main/java/com/kh/bnpp/controller/PayController.java
+++ SpringProjectBnpp/src/main/java/com/kh/bnpp/controller/PayController.java
@@ -8,8 +8,10 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.kh.bnpp.model.biz.ClassBiz;
+import com.kh.bnpp.model.biz.MemberBiz;
 import com.kh.bnpp.model.biz.PayBiz;
 import com.kh.bnpp.model.dto.ClassDto;
+import com.kh.bnpp.model.dto.MemberDto;
 import com.kh.bnpp.model.dto.PayDto;
 
 @Controller
@@ -23,6 +25,9 @@ public class PayController {
 	@Autowired
 	private ClassBiz c_biz;
 	
+	@Autowired
+	private MemberBiz m_biz;
+	
 	@RequestMapping("/paytest.do")
 	public String paytest(Model model, PayDto dto) {
 		model.addAttribute("dto", dto);
@@ -32,6 +37,8 @@ public class PayController {
 	@RequestMapping("/paysuccess.do")
 	public String paysuccess(Model model, String member_id, int class_num) {
 		ClassDto c_dto = c_biz.selectOne(class_num);
+		MemberDto m_dto = m_biz.selectOne(c_dto.getTeacher_id());
+		c_dto.setMember_category(m_dto.getMember_category());
 		PayDto p_dto = new PayDto(c_dto.getClass_price(), member_id, c_dto.getClass_num(),
 									c_dto.getClass_title(), c_dto.getMember_category());
 		
@@ -43,66 +50,6 @@ public class PayController {
 		return "redirect:classdetailteacher.do?teacher_id="+c_dto.getTeacher_id();
 	}
 	
-	@RequestMapping("/mypaylist.do")
-	public String selectMyList(Model model, String member_id) {
-		model.addAttribute("list", biz.selectMyList(member_id));	
-		return "mypaylist";
-	}
-	
-	@RequestMapping("/paylist.do")
-	public String selectList(Model model) {
-		model.addAttribute("list", biz.selectList());	
-		return "paylist";
-	}
-	
-	@RequestMapping("/payselect.do")
-	public String selectOne(Model model, int pay_num) {
-		
-		model.addAttribute("dto", biz.selectOne(pay_num));
-		return "payselect";
-	}
-	
-	@RequestMapping("/payinsert.do")
-	public String insert() {
-		return "payinsert";
-	}
-	
-	@RequestMapping("/payinsertres.do")
-	public String insertres(PayDto dto) {
-		
-		if (biz.insert(dto) > 0) {
-			return "redirect:paylist.do";
-		}
-		
-		return "redirect:payinsert.do";
-	}
-	
-	@RequestMapping("/payupdate.do")
-	public String update(Model model, int pay_num) {
-		
-		model.addAttribute("dto", biz.selectOne(pay_num));
-		
-		return "payupdate";
-	}
-	
-	@RequestMapping("/payupdateres.do")
-	public String updateres(PayDto dto) {
-		
-		if (biz.update(dto) > 0) {
-			return "redirect:payselect.do?pay_num="+dto.getPay_num();
-		}
-		
-		return "redirect:payupdate.do?pay_num="+dto.getPay_num();
-	}
-	
-	@RequestMapping("/paydelete.do")
-	public String delete(int pay_num) {
-		
-		if (biz.delete(pay_num) > 0) {
-			return "redirect:paylist.do";
-		}
-		
-		return "redirect:payselect.do?pay_num="+pay_num;
-	}
 	
 }
+
