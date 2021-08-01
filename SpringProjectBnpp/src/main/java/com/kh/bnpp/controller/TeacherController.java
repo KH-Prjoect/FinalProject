92ca88a08ab20ddd3ef8e724ac55894024741689
insub2004 <55538952+insub2004@users.noreply.github.com>
2021-08-02 01:12:36 +0900

    동영상 업로드

--- SpringProjectBnpp/src/main/java/com/kh/bnpp/controller/TeacherController.java
+++ SpringProjectBnpp/src/main/java/com/kh/bnpp/controller/TeacherController.java
@@ -1,7 +1,12 @@
 package com.kh.bnpp.controller;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
+
+import javax.servlet.http.HttpServletRequest;
 
 import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -57,11 +62,53 @@ public class TeacherController {
 	}
 	
 	@RequestMapping("/classres.do")
-	public String classRes(ClassDto dto, @RequestParam(value="file", required = false) MultipartFile fm, @RequestParam("category") String category, Model model) {
+	public String classRes(ClassDto dto, @RequestParam(value="file", required = false) MultipartFile fm, @RequestParam("category") String category, Model model, HttpServletRequest request) {
 		
 		logger.info("[teacherController] : 강의 등록 res");
 		System.out.println("ClassDto 잘 가져왐? : " + dto);
 		System.out.println("Category 잘 가져왐? : " + category);
+		System.out.println("강의 입력하는 teacher id : " + dto.getTeacher_id());
+		
+		//여기부터 동영상 업로드
+			
+		
+				
+		//동영상 파일 업로드
+		if (!fm.getOriginalFilename().isEmpty()) {
+			
+			String uploadRealPath = request.getSession().getServletContext().getRealPath("/resources/upload/video");
+			
+			File dir = new File(uploadRealPath);
+			if(!dir.exists()) {
+				dir.mkdirs();
+			}
+			
+			// 파일명
+			String originalFile = fm.getOriginalFilename();
+			System.out.println("파일명이 뭔데? : " + originalFile);
+
+			// 파일명 중 확장자만 추출 //lastIndexOt(".") - 뒤에 .위치의 인덱스 번호
+			String originalFileExt = originalFile.substring(originalFile.lastIndexOf("."));
+			// pen.png
+			// lastIndexOf(".") = 3
+			// substring(lastIndexOf(".") = 3) = .png
+
+			// UUID클래스 - (특수문자를 포함한)문자를 랜덤으로 생성, 중복된 이름으로 저장되지 않도록 하기 위해서
+			// db에 저장될 file의 이름 + .확장자
+			String saveFileName = UUID.randomUUID().toString().replaceAll("-", "") + originalFileExt;
+			System.out.println("DB에 저장될 file의 이름 : " + saveFileName);
+			String fullPathName = uploadRealPath + "/" + saveFileName;
+			
+			try {
+				fm.transferTo(new File(uploadRealPath + "/" + saveFileName));
+				dto.setClass_media_name(saveFileName);
+				dto.setClass_media_path(fullPathName);
+				System.out.println("강의 추가할 떄 강의 동영상 추가 성공!");
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		//여기까지 동영상 파일 업로드
 		
 		int res = cBiz.insert(dto);
 		
