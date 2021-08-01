e230cd128315833233c3856b143abf349ff5f107
GwanWooPark <77375127+GwanWooPark@users.noreply.github.com>
2021-08-01 22:35:26 +0900

    Add files via upload

--- SpringProjectBnpp/src/main/java/com/kh/bnpp/model/biz/MemberBiz.java
+++ SpringProjectBnpp/src/main/java/com/kh/bnpp/model/biz/MemberBiz.java
@@ -1,13 +1,29 @@
+
 package com.kh.bnpp.model.biz;
 
+import java.util.List;
+
 import com.kh.bnpp.model.dto.MemberDto;
+import com.kh.bnpp.model.dto.PagingDto;
 
 public interface MemberBiz {
 
-	public MemberDto login(MemberDto dto);	//로그인
-	public int insert(MemberDto dto);		//회원가입
-	public int delete(MemberDto dto);		//회원탈퇴
-	public String checkId(String checkID);  //아이디 중복확인
-	public String findId(MemberDto dto); //아이디 찾기
-	public String findPw(MemberDto dto); //비밀번호 찾기
+	public MemberDto login(MemberDto dto);	
+	public int insert(MemberDto dto);		
+	public int delete(MemberDto dto);		
+	public String checkId(String checkID);  
+	public String findId(MemberDto dto); 
+	public String findPw(MemberDto dto); 
+	public List<MemberDto> findAllT(String category); 
+
+
+	public List<MemberDto> selectList();
+	public MemberDto selectOne(String member_id);
+	public int updatestudent(MemberDto dto);
+	public int updateteacher(MemberDto dto);
+	public int updatepw(MemberDto dto);
+
+	public int countMember(PagingDto pdto);
+	public List<MemberDto> selectMember(PagingDto pdto);
 }
+
--- SpringProjectBnpp/src/main/java/com/kh/bnpp/model/biz/MemberBizImpl.java
+++ SpringProjectBnpp/src/main/java/com/kh/bnpp/model/biz/MemberBizImpl.java
@@ -1,5 +1,9 @@
+
 package com.kh.bnpp.model.biz;
 
+import java.util.List;
+
+import com.kh.bnpp.model.dto.PagingDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -46,6 +50,44 @@ public class MemberBizImpl implements MemberBiz{
 		return dao.findPw(dto);
 	}
 	
+	@Override
+	public List<MemberDto> selectList() {
+		return dao.selectList();
+	}
+
+	@Override
+	public MemberDto selectOne(String member_id) {
+		return dao.selectOne(member_id);
+	}
 	
+	@Override
+	public int updatestudent(MemberDto dto) {
+		return dao.updatestudent(dto);
+	}
 	
+	@Override
+	public int updateteacher(MemberDto dto) {
+		return dao.updateteacher(dto);
+	}
+
+	@Override
+	public List<MemberDto> findAllT(String category) {
+		return dao.findAllT(category);
+	}
+
+	@Override
+	public int updatepw(MemberDto dto) {
+		return dao.updatepw(dto);
+	}
+
+	@Override
+	public int countMember(PagingDto pdto) {
+		return dao.countMember(pdto);
+	}
+
+	@Override
+	public List<MemberDto> selectMember(PagingDto pdto) {
+		return dao.selectMember(pdto);
+	}
 }
+
